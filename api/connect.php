4f68cb9613ef4ec19366d558e476220d54745945
oupen <oupen@users.noreply.github.com>
2017-04-02 22:12:37 +0200

    Update connect.php

--- api/connect.php
+++ api/connect.php
@@ -1,8 +1,8 @@
 <?
-	$user = "baptistewtadmin";
-	$psw = "Alakay0202";
-    $host= "baptistewtadmin.mysql.db";
-    $base= "baptistewtadmin";
+	$user = "root";
+	$psw = "";
+    $host= "";
+    $base= "";
 	$bdd = mysqli_connect($host,$user,$psw); mysqli_select_db($bdd,$base) or die($erreur="Erreur lors de la connection à la bdd");
 	echo $erreur;
 	mysqli_set_charset($bdd,"utf8");
